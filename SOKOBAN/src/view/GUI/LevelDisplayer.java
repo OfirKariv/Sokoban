c8707e4a672635a515d0303e1674cb39876af59e
omer_ <omer_@DESKTOP-BS062LQ>
2017-01-23 14:58:09 +0200

    gui

---
+++ SOKOBAN/src/view/GUI/LevelDisplayer.java
@@ -0,0 +1,12 @@
+package view.GUI;
+
+import javafx.scene.canvas.Canvas;
+import model.data.util.Level;
+
+public class LevelDisplayer extends Canvas {
+
+    public void display(Level outLevel) {
+
+    }
+
+}
